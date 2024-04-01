7f745569615888ca24e934e6df322e1f9dc1738f
Igor Brusko <tailsmiles2015b@gmail.com>
2024-04-01 19:59:34 +0700

    Промежуточный

---
+++ Assets/Scripts/EquipmentController.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentController : MonoSingleton<EquipmentController>
+{
+    private int _indexWeapons;
+    private int _indexShields;
+    private int _indexArmors;
+    private int _indexAccessories;
+    private int _indexHeadArmors;
+    private int _indexHats;
+    
+    [field: SerializeField] public GameObject Hair { get; private set; }
+    
+    [field: SerializeField] public List<GameObject> Weapons { get; private set; }
+    [field: SerializeField] public List<GameObject> Shields { get; private set; }
+    [field: SerializeField] public List<GameObject> Armors { get; private set; }
+    [field: SerializeField] public List<GameObject> Accessories { get; private set; }
+    [field: SerializeField] public List<GameObject> HeadArmors { get; private set; }
+    [field: SerializeField] public List<GameObject> Hats { get; private set; }
+
+    public void ChangeEquipment(EquipmentType equipmentType, int index)
+    {
+        var equipmentGameObjects = new List<GameObject>();
+        switch (equipmentType)
+        {
+            case EquipmentType.Weapon:
+                equipmentGameObjects = Weapons;
+                break;
+            case EquipmentType.Shield:
+                equipmentGameObjects = Shields;
+                break;
+            case EquipmentType.Armor:
+                equipmentGameObjects = Armors;
+                break;
+            case EquipmentType.Accessory:
+                equipmentGameObjects = Accessories;
+                break;
+            case EquipmentType.HeadArmor:
+                equipmentGameObjects = HeadArmors;
+                break;
+            case EquipmentType.Hat:
+                equipmentGameObjects = Hats;
+                break;
+        }
+
+        foreach (var equipmentGameObject in equipmentGameObjects)
+        {
+            equipmentGameObject.SetActive(equipmentGameObjects.IndexOf(equipmentGameObject) == index);
+        }
+    } 
+}
+
+public enum EquipmentType
+{
+    Weapon,
+    Shield,
+    Armor,
+    Accessory,
+    HeadArmor,
+    Hat
+}
