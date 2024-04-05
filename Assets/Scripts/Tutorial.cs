e2a4a6f548bd722281e146426dfddcaba320ec87
TailsMiles <tailsmiles2015b@gmail.com>
2024-04-06 02:20:33 +0700

    Сделал туториал

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,38 +1,61 @@
 using System.Collections;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using YG;
 
 public class Tutorial : MonoSingleton<Tutorial>
 {
     [SerializeField] private Image _attackTutorImage;
+    [SerializeField] private TMP_Text _attackTutorText;
     
     [SerializeField] private Image _upgradeTutorImage;
+    [SerializeField] private TMP_Text _upgradeTutorText;
     
     [SerializeField] private Image _nextEnemyTutorImage;
+    [SerializeField] private TMP_Text _nextEnemyTutorText;
+    
+    [SerializeField] private Image _nextEnemyTutorArrowImage;
     [SerializeField] private Transform _nextEnemyTutorArrow;
     
-    private void Start()
+    public void OnEnable()
     {
-        if (!SaveManager.Instance.LoadTutorialState())
+        if (!YandexGame.savesData.TutorialComplete)
         {
             Sequence sequence = DOTween.Sequence();
             sequence.Append(_attackTutorImage.DOFade(1, 1f));
-            sequence.PrependInterval(2);
-            sequence.Append(_attackTutorImage.DOFade(0, 0.2f));
+            sequence.Join(_attackTutorText.DOFade(1, 1f));
+            sequence.Append(_attackTutorImage.transform.DOScale(1.2f, 1f));
+            sequence.Append(_attackTutorImage.DOFade(0, 0.6f));
+            sequence.Join(_attackTutorText.DOFade(0, 0.6f));
         
             sequence.Append(_upgradeTutorImage.DOFade(1, 1f));
-            sequence.PrependInterval(2);
-            sequence.Append(_upgradeTutorImage.DOFade(0, 0.2f));
+            sequence.Join(_upgradeTutorText.DOFade(1, 1f));
+            sequence.Append(_upgradeTutorImage.transform.DOScale(1.2f, 1f));
+            sequence.Append(_upgradeTutorImage.DOFade(0, 0.6f));
+            sequence.Join(_upgradeTutorText.DOFade(0, 0.6f));
 
             Sequence arrowSequence = DOTween.Sequence();
-            arrowSequence.Append(_nextEnemyTutorArrow.DOScale(1.4f, 1.4f));
-            arrowSequence.Append(_nextEnemyTutorArrow.DOScale(1, 0.4f));
-            arrowSequence.SetLoops(-1);
+            arrowSequence.SetLoops(3);
         
             sequence.Append(_nextEnemyTutorImage.DOFade(1, 1f));
-            sequence.PrependInterval(2);
-            sequence.Append(_nextEnemyTutorImage.DOFade(0, 0.2f));
+            sequence.Join(_nextEnemyTutorText.DOFade(1, 1f));
+            sequence.Join(_nextEnemyTutorArrowImage.DOFade(1f, 1.4f));
+            
+            sequence.Append(_nextEnemyTutorImage.transform.DOScale(1.2f, 1f));
+            
+            sequence.Join(arrowSequence.Append(_nextEnemyTutorArrow.DOScale(1.4f, 1.4f)));
+            sequence.Join(arrowSequence.Append(_nextEnemyTutorArrow.DOScale(1, 0.8f)));
+            
+            sequence.Append(_nextEnemyTutorImage.DOFade(0, 0.6f));
+            sequence.Join(_nextEnemyTutorText.DOFade(0, 0.6f));
+            sequence.Join(_nextEnemyTutorArrowImage.DOFade(0, 0.6f));
+
+            sequence.onComplete += () =>
+            {
+                SaveManager.Instance.TutorialComplete();
+            };
         }
     }
 }
